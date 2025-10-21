4a11da80895cfbdeed70a33beb49024e66619626
Belen Medina Roldan <bmedinaroldan@gmail.com>
2025-10-21 13:46:30 -0300

    Modify domain service

--- domain/src/services/loan-policy.service.ts
+++ domain/src/services/loan-policy.service.ts
@@ -3,4 +3,5 @@ import { User, Loan } from '../entities/index';
 export interface LoanPolicyService {
     canUserBorrow(user: User, activeLoans: Loan[]): boolean;
     isLoanOverdue(loan: Loan): boolean;
+    getLoanLimitByRole(roleName: string): number;
 }
