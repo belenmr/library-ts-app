71459ea799c667f62458822c7979a3b836bf7a8c
Belen Medina Roldan <bmedinaroldan@gmail.com>
2025-10-29 18:26:42 -0300

    fix(domain): add findAll and findByRole in User Repository

--- domain/src/repositories/user.repository.ts
+++ domain/src/repositories/user.repository.ts
@@ -1,9 +1,11 @@
-import { User } from '../entities/index';
+import { RoleName, User } from '../entities/index';
 
 export interface UserRepository {
 	//Lectura
 	findById(id: string): Promise<User | null>;
 	findByEmail(email: string): Promise<User | null>;
+	findByRole(roleName: RoleName): Promise<User[]>;
+	findAll(): Promise<User[]>;
 
 	//Escritura
 	save(user: User): Promise<void>;
